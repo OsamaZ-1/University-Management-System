b08031500a550767d840fcd3feee6a2f1084bbfc
Osama Zammar <zammarosama@gmail.com>
2023-01-07 13:09:50 -0800

    added function submitChanges() but no functionallity yet, I am sleepy now, I'll do it tommorow

--- Controller/AdminPanelController.java
+++ Controller/AdminPanelController.java
@@ -3,6 +3,8 @@ package Controller;
 import java.sql.SQLException;
 import javax.swing.JCheckBox;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import Model.Student;
 import View.AdminPanel;
@@ -36,5 +38,15 @@ public class AdminPanelController {
         }catch(SQLException e1){System.out.println(e1.getStackTrace());}
     }
 
-    
+    public void submitChanges(){
+        adminPanelView.getSubmitButton().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                //this allows getting the value of the checkbox at i, j
+                //adminPanelView.getActivityTable().getModel().getValueAt(i, j)
+
+                //this function wil accept the students that the admin checked
+            }
+        });
+    }
 }
