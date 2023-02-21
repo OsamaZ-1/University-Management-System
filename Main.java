e1740557c6eb85d66f4f6f1ecd0acc74c98f6e22
Osama Zammar <zammarosama@gmail.com>
2023-02-21 06:26:34 -0800

    documents folder + class uml

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ import View.StudentTranscriptView;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-    	new InstructorController("moe@dandash.com","php123");
-        //new AdminPanelController();
+    	//new InstructorController("moe@dandash.com","php123");
+        new AdminPanelController();
         //new RegisterController();
         //new AdminPanelController();
         //new RegisterController();
